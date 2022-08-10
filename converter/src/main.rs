704b6658938ed9f368bc00b7c5c656f27acec163
Rian-Tan <tan_jun_yu_rian@s2021.ssts.edu.sg>
2022-08-10 17:34:28 +0800

    minor changes ;)

--- converter/src/main.rs
+++ converter/src/main.rs
@@ -1,15 +1,57 @@
 use std::io;
 
 fn main() {
-    let mut plaintext = String::new();
-    println!("Enter plaintext string");
-    io::stdin()
-        .read_line(&mut plaintext)
-        .expect("failed to read line");
-    
-    println!("Your encrypted string:");
-    for _i in 1..plaintext.chars().count(){
-        print!("quack ");
+    let mut a: i8 = 1;
+    while a == 1{
+
+        let mut plaintext = String::new();
+        println!("Enter plaintext string");
+        io::stdin()
+            .read_line(&mut plaintext)
+            .expect("failed to read line");
+        plaintext.retain(|c| !c.is_whitespace());
+        
+
+
+        if plaintext.to_lowercase() == "quack"{
+            println!("\nended program successfully");
+            a = 0;
+        } else{
+            println!("Your encrypted string: ");
+            let str_vec: Vec<char> = plaintext.to_lowercase().chars().collect();
+            for i in 0..plaintext.chars().count() {
+                match str_vec[i] {
+                    'a' => print!("quack "),
+                    'b' => print!("qUack "),
+                    'c'=> print!("quAck "),
+                    'd' => print!("quaCk "),
+                    'e' => print!("quacK "),
+                    'f' => print!("Quack "),
+                    'g' => print!("QUack "),
+                    'h' => print!("QuAck "),
+                    'i' => print!("QuaCk "),
+                    'j' => print!("QuacK "),
+                    'k' => print!("QUAck "),
+                    'l' => print!("QUaCk "),
+                    'm' => print!("QUacK "),
+                    'n' => print!("QuACk "),
+                    'o' => print!("QuAcK "),
+                    'p' => print!("QuaCK "),
+                    'q' => print!("QUACk "),
+                    'r' => print!("QUACK "),
+                    's' => print!("QuACK "),
+                    't' => print!("QUaCK "),
+                    'u' => print!("QUAcK "),
+                    'v' => print!("QUACkk "),
+                    'w' => print!("qUACK "),
+                    'x' => print!("qUaCk "),
+                    'y' => print!("quacck "),
+                    'z' => print!("quaack "),
+                    _ => print!("quacc "),
+                }
+            }
+            println!(" ");
+        }
     }
     println!(" ");
 
